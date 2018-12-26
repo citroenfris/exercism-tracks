af876d1523bb31b6d963d59d78554cd74e340bea
Niels Willems <niels.willems88@gmail.com>
2018-12-26 14:47:13 +0100

    Added isogram comments

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -5,23 +5,30 @@ import (
 )
 
 // IsIsogram determines if a word is an isogram or not
+// It compares the length of a string together with the length of the unique values of that string.
+// If the length of the word is longer than the count of unique values it is not an isogram
 func IsIsogram(word string) bool {
 	wordCharacters := []rune(strings.ToLower(word))
-	uniqueSlice := getUniqueValues(wordCharacters)
-	return len(wordCharacters) == len(uniqueSlice)
+	uniqueLetters := getUniqueLetters(wordCharacters)
+	return len(wordCharacters) == len(uniqueLetters)
 }
 
-func getUniqueValues(runeSlice []rune) []rune {
+// getUniqueValues returns all unique characters in the given slice
+// Whitespace value and - are not required to be unique.
+func getUniqueLetters(inputSlice []rune) []rune {
 	keys := make(map[rune]bool)
-	list := []rune{}
-	for _, entry := range runeSlice {
-		if entry == ' ' || entry == '-' {
-			list = append(list, entry)
+	uniqueLetters := []rune{}
+	for _, char := range inputSlice {
+		// These characters are allowed to duplicate so add them to the uniqueLetters slice
+		if char == ' ' || char == '-' {
+			uniqueLetters = append(uniqueLetters, char)
 			continue
-		} else if _, value := keys[entry]; !value {
-			keys[entry] = true
-			list = append(list, entry)
+		}
+		// If the char is not yet present in the uniqueLetters slice map it can be added to the uniqueLetters slice.
+		if _, value := keys[char]; !value {
+			keys[char] = true
+			uniqueLetters = append(uniqueLetters, char)
 		}
 	}
-	return list
+	return uniqueLetters
 }
