fdf7652dfb6a3aeac840b408580eead543ee069f
Niels Willems <niels.willems88@gmail.com>
2018-12-26 18:10:01 +0100

    updated isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -9,26 +9,15 @@ import (
 // If the length of the word is longer than the count of unique values it is not an isogram
 func IsIsogram(word string) bool {
 	wordCharacters := []rune(strings.ToLower(word))
-	uniqueLetters := getUniqueLetters(wordCharacters)
-	return len(wordCharacters) == len(uniqueLetters)
-}
-
-// getUniqueValues returns all unique characters in the given slice
-// Whitespace value and - are not required to be unique.
-func getUniqueLetters(inputSlice []rune) []rune {
 	keys := make(map[rune]bool)
-	uniqueLetters := []rune{}
-	for _, char := range inputSlice {
-		// These characters are allowed to duplicate so add them to the uniqueLetters slice
+	for _, char := range wordCharacters {
 		if char == ' ' || char == '-' {
-			uniqueLetters = append(uniqueLetters, char)
 			continue
 		}
-		// If the char is not yet present in the uniqueLetters slice map it can be added to the uniqueLetters slice.
-		if _, value := keys[char]; !value {
-			keys[char] = true
-			uniqueLetters = append(uniqueLetters, char)
+		if keys[char] {
+			return false
 		}
+		keys[char] = true
 	}
-	return uniqueLetters
+	return true
 }
